acf6f4857e2295aa13c6a863b86db2fc906c1da3
Nicholas Petrik <frokrab@gmail.com>
2018-05-15 09:20:08 -0700

    Created MovieList

---
+++ src/components/MoveList.jsx
@@ -0,0 +1,8 @@
+var MovieList = (props) => (
+  <div>
+    {props.movies.map((movie) => 
+    <div> Insert movie here! </div>)}
+  </div>
+);
+
+window.MovieList = MovieList;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
-var App = () => (
+var App = (props) => (
   <div>
     <h2>Movie List</h2>
+    <MovieList movies={movies} />
   </div>
 );
