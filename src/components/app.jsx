b8ef3086c4ebfdabdc92b07ebf46a83fd9343bc5
Nicholas Petrik <frokrab@gmail.com>
2018-05-15 18:11:13 -0700

    Refactored App to be a functional state class

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,20 @@
-var App = (props) => (
-  <div>
-    <h2 className="page-title">Movie List</h2>
-    <Search />
-    <div>
-      <MovieList movies={movies} />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collection: window.movies
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h2 className="page-title">Movie List</h2>
+        <Search />
+        <div>
+          <MovieList movies={this.state.collection} />
+        </div>
+      </div>
+    );
+  }
+}
