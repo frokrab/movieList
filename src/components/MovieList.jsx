f8f6ea865f2ab7d606795b7b94f7fc3fdb0e83b9
Nicholas Petrik <frokrab@gmail.com>
2018-05-17 13:26:19 -0700

    Passed indices to MovieListEntries

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,7 @@
 var MovieList = (props) => (
   <div>
-    {props.movies.map((movie) => 
-    <MovieListEntry movie={movie} />)}
+    {props.movies.map((movie, index) => 
+      <MovieListEntry movie={movie} key={index}/>)}
   </div>
 );
 
